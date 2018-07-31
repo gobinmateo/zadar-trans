c5360241dbb055268d177ec5a1ba6d02dac7d814
mateogobin <mateo.gobin@coreline.agency>
2018-07-31 18:51:31 +0200

    added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import LoginPage from './Components/LoginPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <LoginPage/>
       </div>
     );
   }
