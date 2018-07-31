923805ab67033cffd0663655adb08ad0072c2411
mateogobin <mateo.gobin@coreline.agency>
2018-07-31 19:39:44 +0200

    installed material ui core

--- client/src/Components/LoginPage.js
+++ client/src/Components/LoginPage.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import Button from "@material-ui/core/Button/Button";
 
 export default class LoginPage extends Component {
     render() {
         return (
-            <div>
-                Lazanja je glup
+            <div className="login-container justify-content-center">
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
             </div>
         )
     }
-}
+};
+
