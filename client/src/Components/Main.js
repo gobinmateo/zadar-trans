7b6b60a1822d70bc79bce7245ece0564c064631d
mateogobin <mateo.gobin@coreline.agency>
2018-07-31 20:40:20 +0200

    added routing

---
+++ client/src/Components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import LoginPage from './LoginPage';
+import Home from './Home';
+
+const Main = () => (
+    <main>
+        <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/login' component={LoginPage}/>
+        </Switch>
+    </main>
+);
+
+export default Main;
