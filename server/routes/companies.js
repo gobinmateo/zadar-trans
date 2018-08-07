3f9a701984dc8a347be641057cb82f0121623ff8
Luka Lazanja <llazanja96@gmail.com>
2018-08-07 19:25:53 +0200

    Company route refactor, added population

--- server/routes/companies.js
+++ server/routes/companies.js
@@ -1,97 +1,77 @@
 import express from 'express';
+import updateAttributesFromParams from '../utils/paramsParser';
 
 import Company from '../models/company.model';
+import User from '../models/user.model';
 
 const router = express.Router();
 
 router.delete('/', async (req, res, next) => {
   await Company.deleteMany();
 
-  res.sendStatus(200);
+  res.sendStatus(204);
 });
 
 router.delete('/:id', async (req, res, next) => {
-  const id = req.params.id;
+  const resp = await Company.deleteOne({ id: req.params.id });
 
-  if(id === undefined) res.send({ error: 'Company id has to be provided!' });
-
-  await Company.deleteOne({ id });
-
-  res.sendStatus(200);
+  // if resource does not exist, return 404, else 204
+  resp.n === 0 ? res.sendStatus(404) : res.sendStatus(204);
 });
 
 router.get('/', async (req, res, next) => {
-  const companies = await Company.find();
+  const companies = await Company.find()
+                                  .populate('users');
 
   res.json(companies);
 });
 
 router.get('/:id', async (req, res, next) => {
-  const id = req.params.id;
-
-  if(id === undefined) res.send({ error: 'Id has to be provided!' });
-
-  const company = await Company.findOne({ id });
+  const company = await Company.findOne({ id: req.params.id })
+                                .populate('users');
 
   if(!company) {
-    res.status(403).send({ error: 'Company with provided id does not exist!' });
+    res.sendStatus(404);
   } else {
     res.json(company);
   }
 });
 
-router.put('/', async (req, res, next) => {
-  const { contractExpiryDate, id, name, users } = req.body;
+router.put('/:id', async (req, res, next) => {
+  const company = await Company.findOne({ id: req.params.id });
 
-  // both parameters have to be present
-  if(password === undefined) {
-    res.status(400).send({ error: 'Password has to be provided!' });
-  }
-
-  const company = await Company.findOne({ id });
-
-  // company doesn't exist
   if(!company) {
-    res.status(403).send({ error: 'Company with provided id does not exist!' });
+    res.sendStatus(404);
   } else {
-    company.contractExpiryDate = contractExpiryDate;
-    company.name = name;
-    company.users = users;
+    updateAttributesFromParams(req.body, company);
 
     await company.save();
 
-    res.status(200).send({ message: 'Company successfully updated.' });
+    res.sendStatus(200);
   }
 });
 
 router.post('/', async (req, res, next) => {
-  const { contractExpiryDate, id, name, users } = req.body;
+  const company = await Company.findOne({ id: req.body.id });
 
-  // both parameters have to be present
-  if(id === undefined) {
-    res.status(400).send({ error: 'Company id has to be provided!' });
-  }
-
-  const company = await Company.findOne({ id });
-
-  // company already exists
   if(company) {
-    res.status(403).send({ error: 'Company with provided id already exists!' });
+    res.sendStatus(400);
   } else {
-    const hash = crypto.createHash('sha256');
-
-    hash.update(password);
-
     const newCompany = new Company();
 
-    newCompany.contractExpiryDate = contractExpiryDate;
-    newCompany.id = id;
-    newCompany.name = name;
-    newCompany.users = users;
+    updateAttributesFromParams(req.body, newCompany);
+    newCompany.users = [];
 
-    await newCompany.save();
+    for(const email of req.body.users) {
+      const user = await User.findOne({ email });
 
-    res.status(200).send({ message: 'Company successfully added to database.' });
+      if(user) newCompany.users.push(user._id);
+    }
+
+    await newCompany.save((err => {
+      if(err) res.status(400).send(err.errors.id.message);
+      else res.sendStatus(201);
+    }));
   }
 });
 
